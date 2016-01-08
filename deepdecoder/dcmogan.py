9148f3653590583cd02efe11cea1a1559810dbb7
berleon <github@leon-sixt.de>
2016-01-09 00:49:00 +0100

    extract dcmogan into its own module

--- deepdecoder/dcmogan.py
+++ deepdecoder/dcmogan.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Leon Sixt
+# Copyright 2016 Leon Sixt
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
 import os
 import time
 
@@ -20,11 +22,6 @@ from beesgrid.pybeesgrid import NUM_CONFIGS, NUM_MIDDLE_CELLS
 from beras.gan import GAN
 from beesgrid import TAG_SIZE, CONFIG_ROTS
 from beras.models import asgraph
-from keras import initializations
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import Convolution2D
-from keras.layers.core import Dense, Layer, Reshape, Activation, Flatten
-from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential, Graph
 from keras.objectives import mse
 from keras.optimizers import Adam
@@ -35,93 +32,6 @@ from deepdecoder.mogan import MOGAN
 from deepdecoder.utils import loadRealData, binary_mask
 import numpy as np
 from deepdecoder.keras_fix import Convolution2D as TheanoConvolution2D
-
-
-class Deconvolution2D(Layer):
-    def __init__(self, nb_filter, nb_row, nb_col,
-                 subsample=(1, 1), border_mode=(0, 0), init='glorot_uniform', **kwargs):
-        self.nb_filter = nb_filter
-        self.nb_row = nb_row
-        self.nb_col = nb_col
-        self.init = initializations.get(init)
-        self.border_mode = border_mode
-        self.subsample = subsample
-        super(Deconvolution2D, self).__init__(**kwargs)
-
-    def build(self):
-        stack_size = self.input_shape[1]
-        self.W_shape = (stack_size, self.nb_filter, self.nb_row, self.nb_col)
-        self.W = self.init(self.W_shape)
-        self.params = [self.W]
-
-    @property
-    def output_shape(self):
-        in_rows, in_cols = self.input_shape[2:]
-        sr, sc = self.subsample
-        return self.input_shape[0], self.nb_filter, sr*in_rows, sr*in_cols
-
-    def get_output(self, train=False):
-        """
-        sets up dummy convolutional forward pass and uses its grad as deconv
-        currently only tested/working with same padding
-        """
-        img = self.get_input(train)
-        kerns = self.W
-        sr, sc = self.subsample
-        out = gpu_alloc_empty(img.shape[0], kerns.shape[1], img.shape[2]*sr, img.shape[3]*sc)
-        desc = GpuDnnConvDesc(
-            border_mode=self.border_mode, subsample=self.subsample,
-            conv_mode='conv')(out.shape, kerns.shape)
-        d_img = GpuDnnConvGradI()(kerns, img, out, desc)
-        return d_img
-
-
-def generator(input_dim=50, nb_output_channels=1):
-    n = 64
-    model = Sequential()
-    model.add(Dense(8*n*4*4, input_dim=input_dim))
-    model.add(Reshape((8*n, 4, 4,)))
-
-    model.add(Deconvolution2D(4*n, 5, 5, subsample=(2, 2), border_mode=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-
-    model.add(Deconvolution2D(2*n, 5, 5, subsample=(2, 2), border_mode=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-
-    model.add(Deconvolution2D(n, 5, 5, subsample=(2, 2), border_mode=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-
-    model.add(Deconvolution2D(nb_output_channels, 5, 5, subsample=(2, 2),
-                              border_mode=(2, 2)))
-    model.add(Activation('sigmoid'))
-    return model
-
-
-def discriminator():
-    n = 64
-    model = Sequential()
-    model.add(TheanoConvolution2D(n, 5, 5, subsample=(2, 2), border_mode='full',
-                            input_shape=(1, 64, 64)))
-    model.add(LeakyReLU(0.2))
-
-    model.add(TheanoConvolution2D(2*n, 5, 5, subsample=(2, 2), border_mode='full'))
-    model.add(BatchNormalization())
-    model.add(LeakyReLU(0.2))
-
-    model.add(TheanoConvolution2D(4*n, 5, 5, subsample=(2, 2), border_mode='full'))
-    model.add(BatchNormalization())
-    model.add(LeakyReLU(0.2))
-
-    model.add(TheanoConvolution2D(8*n, 5, 5, subsample=(2, 2), border_mode='full'))
-    model.add(BatchNormalization())
-    model.add(LeakyReLU(0.2))
-
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
-    return model
 
 
 def dcmogan(generator_fn, discriminator_fn, batch_size=128):
