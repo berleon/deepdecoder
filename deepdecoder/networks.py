f144fcac1a435f96066bd0ca5322aa7a9a500436
berleon <github@leon-sixt.de>
2016-01-10 20:55:50 +0100

    add diff gan

--- deepdecoder/networks.py
+++ deepdecoder/networks.py
@@ -13,15 +13,18 @@
 # limitations under the License.
 
 
-from keras.models import Sequential
+from keras.models import Sequential, Graph
 from keras.layers.core import Dense, Dropout, Flatten, Reshape, Activation
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.normalization import BatchNormalization
-from beesgrid import NUM_MIDDLE_CELLS
+from beesgrid import NUM_MIDDLE_CELLS, TAG_SIZE, NUM_CONFIGS, CONFIG_ROTS
 from beras.layers.attention import RotationTransformer
+from beras.gan import GAN
+from beras.models import asgraph
 from deepdecoder.deconv import Deconvolution2D
 from deepdecoder.keras_fix import Convolution2D as TheanoConvolution2D
+from deepdecoder.utils import binary_mask, rotate_by_multiple_of_90
 
 
 def get_decoder_model():
@@ -120,3 +123,35 @@ def dcgan_discriminator():
     model.add(Flatten())
     model.add(Dense(1, activation='sigmoid'))
     return model
+
+
+NB_GAN_GRID_PARAMS = NUM_CONFIGS + NUM_MIDDLE_CELLS + len(CONFIG_ROTS)
+
+
+def diff_gan(generator, discriminator, batch_size=128, nb_z=20):
+    nb_grid_params = NB_GAN_GRID_PARAMS
+    z_shape = (batch_size, nb_z)
+    grid_shape = (1, TAG_SIZE, TAG_SIZE)
+    grid_params_shape = (nb_grid_params, )
+
+    g_graph = Graph()
+    g_graph.add_input('z', input_shape=z_shape[1:])
+    g_graph.add_input('grid_params', input_shape=grid_params_shape)
+    g_graph.add_input('grid_idx', input_shape=grid_shape)
+    g_graph.add_input('rot90', input_shape=(1, ))
+    g_graph.add_node(generator, 'generator', inputs=['z', 'grid_params'])
+    g_graph.add_output('output', input='generator')
+    g_graph.add_output('rot90', input='rot90')
+    g_graph.add_output('grid_idx', input='grid_idx')
+    d_graph = asgraph(discriminator, input_name=GAN.d_input)
+
+    def add_diff(g_outmap):
+        g_out = g_outmap["output"]
+        grid_idx = g_outmap['grid_idx']
+        rot90 = g_outmap['rot90']
+        g_rotate = rotate_by_multiple_of_90(g_out, rot90)
+        alphas = binary_mask(grid_idx, black=0.5, ignore=1.0,  white=0.5)
+        bw_mask = binary_mask(grid_idx, black=0., ignore=0,  white=0.5)
+        return (alphas * g_rotate) + bw_mask
+
+    return GAN(g_graph, d_graph, z_shape, add_diff)
---
+++ test/test_networks.py
@@ -0,0 +1,35 @@
+# Copyright 2015 Leon Sixt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Flatten, Reshape
+from keras.optimizers import Adam
+from beesgrid import TAG_SIZE
+from deepdecoder.networks import diff_gan, NB_GAN_GRID_PARAMS
+
+
+def test_networks_diff_gan():
+    n = TAG_SIZE*TAG_SIZE
+    nb_z = 20
+    generator = Sequential()
+    generator.add(Dense(n, input_dim=nb_z+NB_GAN_GRID_PARAMS))
+    generator.add(Reshape((1, TAG_SIZE, TAG_SIZE)))
+
+    discriminator = Sequential()
+    discriminator.add(Flatten(input_shape=(1, TAG_SIZE, TAG_SIZE)))
+    discriminator.add(Dense(1, input_dim=n))
+
+    gan = diff_gan(generator, discriminator, nb_z=nb_z)
+    gan.compile(Adam(), Adam())
