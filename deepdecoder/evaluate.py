07656e1267e623f5946e0f4ae73bb67051226395
berleon <github@leon-sixt.de>
2016-04-24 15:19:14 +0000

    Add evaluation

---
+++ deepdecoder/evaluate.py
@@ -0,0 +1,70 @@
+# Copyright 2015 Leon Sixt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from beesgrid import gt_grids, NUM_MIDDLE_CELLS, CONFIG_LABELS
+import numpy as np
+
+
+def evaluate_decoder(decoder):
+    pass
+
+
+def mean_hamming_distance(x, y):
+    return np.count_nonzero(x != y) / len(x)
+
+
+def nth_bit_right_accuracy(bits_true, bits_pred, nth):
+    accurate = np.sum(bits_true == bits_pred, axis=1) >= nth
+    return accurate.mean()
+
+
+class GTEvaluator:
+    def __init__(self, gt_files):
+        self.gt_files = gt_files
+        self.batch_size = 32
+
+    def evaluate(self, predict, bit_threshold=0.5):
+        gt_images, bits_true, configs_true = \
+            next(gt_grids(self.gt_files, all=True))
+        nb_samples = len(gt_images)
+        bits_pred = []
+        configs_pred = []
+        for i in range(0, nb_samples, self.batch_size):
+            to = min(i + self.batch_size, nb_samples)
+            batch_bits_pred, batch_configs_pred = predict(gt_images[i:to])
+            threshold_arr = bit_threshold * np.ones_like(batch_bits_pred)
+            batch_bits_pred = batch_bits_pred >= threshold_arr
+            print(batch_bits_pred.shape)
+            print(batch_bits_pred.dtype)
+            batch_bits_pred = batch_bits_pred.astype(np.float32)
+            bits_pred.append(batch_bits_pred)
+            configs_pred.append(batch_configs_pred)
+
+        bits_pred = np.concatenate(bits_pred, axis=0)
+        configs_pred = np.concatenate(configs_pred, axis=0)
+        results = {
+            'mean_hamming_distance': mean_hamming_distance(bits_true,
+                                                           bits_pred)
+        }
+        for i in range(1, NUM_MIDDLE_CELLS + 1):
+            results['accuracy_{}_bits_right'.format(i)] = \
+                nth_bit_right_accuracy(bits_true, bits_pred, i)
+
+        def mse(x, y):
+            return ((x - y)**2).mean()
+
+        for i, label in enumerate(CONFIG_LABELS):
+            name = 'mse_{}'.format(label)
+            results[name] = mse(configs_pred[:, i], configs_true[:, i])
+        return results
---
+++ test/test_evaluation.py
@@ -0,0 +1,29 @@
+# Copyright 2015 Leon Sixt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+from deepdecoder.evaluate import GTEvaluator
+from glob import glob
+from beesgrid import NUM_MIDDLE_CELLS, CONFIG_LABELS
+
+
+def test_evaluation():
+    def predict(x):
+        return np.random.uniform(size=(len(x), NUM_MIDDLE_CELLS)), \
+               np.random.uniform(size=(len(x), len(CONFIG_LABELS)))
+
+    gt_dir = '/home/leon/repos/deeplocalizer_data/images/season_2015'
+    gt_files = glob(gt_dir + '/**/*.tdat', recursive=True)
+    evaluator = GTEvaluator(gt_files)
+    results = evaluator.evaluate(predict)
