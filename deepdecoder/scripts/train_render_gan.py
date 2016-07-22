f290faebfe6ebb84320810ae9857138baeb30817
berleon <github@leon-sixt.de>
2016-07-22 15:14:56 +0000

    Add train_render_gan script

---
+++ deepdecoder/scripts/train_render_gan.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python3
+# Copyright 2016 Leon Sixt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import matplotlib
+matplotlib.use('Agg')  # noqa
+
+from deepdecoder.render_gan import RenderGAN, train, train_callbacks, load_tag3d_network, \
+    save_real_images
+import argparse
+import sys
+import os
+
+sys.setrecursionlimit(10000)
+
+
+def run(output_dir, tag3d_network_weights, real_hdf5_fname, force, nb_epoch,
+        nb_gen_units, nb_dis_units):
+    os.makedirs(output_dir, exist_ok=force)
+    visualise_dir = os.path.join(output_dir, 'visualise')
+    os.makedirs(visualise_dir, exist_ok=force)
+    save_real_images(real_hdf5_fname, visualise_dir)
+
+    tag3d_network = load_tag3d_network(tag3d_network_weights)
+    labels_shape = tag3d_network.input_layers[0].batch_input_shape
+    gan = RenderGAN(tag3d_network, labels_shape=labels_shape[1:])
+    callbacks = train_callbacks(gan, output_dir, 20**2)
+    train(gan, real_hdf5_fname, output_dir, callbacks=callbacks,
+          batch_size=16,
+          nb_epoch=nb_epoch)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Train the mask blending gan')
+
+    parser.add_argument('-r', '--real', type=str,
+                        help='hdf5 file with the real data')
+    parser.add_argument('--nntag3d', type=str,
+                        help='weights of neuronal network trained on genenerated 3d tags')
+    parser.add_argument('--gen-units', default=96, type=int,
+                        help='number of units in the generator.')
+    parser.add_argument('--dis-units', default=64, type=int,
+                        help='number of units in the discriminator.')
+    parser.add_argument('--epoch', default=301, type=int,
+                        help='number of epoch to train.')
+    parser.add_argument('-f', '--force', action='store_true',
+                        help='override existing output files')
+    parser.add_argument('--output-dir', type=str, help='output dir of the gan')
+    args = parser.parse_args()
+
+    run(args.output_dir, args.nntag3d, args.real, args.force,
+        args.epoch, args.gen_units, args.dis_units)
+
+if __name__ == "__main__":
+    main()
